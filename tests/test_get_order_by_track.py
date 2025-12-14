d4366d40923a68a1f87b978146c3e338ef4a3685
Anatoliy Shepovalov <shepovalov_a@mail.ru>
2025-12-14 21:15:54 +0545

    Fix get order test

--- tests/test_get_order_by_track.py
+++ tests/test_get_order_by_track.py
@@ -5,18 +5,15 @@ from configuration import URL_SERVICE, CREATE_ORDER_PATH, GET_ORDER_BY_TRACK_PAT
 from data import order_body
 
 def test_get_order_by_track_returns_200():
-    # Arrange: создаём заказ и берём track
     create_response = requests.post(
         URL_SERVICE + CREATE_ORDER_PATH,
         json=order_body
     )
     track = create_response.json().get("track")
 
-    # Act: получаем заказ по треку
     response = requests.get(
         URL_SERVICE + GET_ORDER_BY_TRACK_PATH,
         params={"t": track}
     )
 
-    # Assert
     assert response.status_code == 200
